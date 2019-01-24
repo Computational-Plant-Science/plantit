9d0a515ec3842aaced0695b77caf9d8728dbf2ba
Chris Cotter <cotter@uga.edu>
2019-01-24 13:17:08 -0500

    Simple changes discussed at 1/24 meeting
    * User landing page now reffered to as Dashboard in front end
    * Removed back buttons
    * moved actions buttions to top of page
    * added Cancel or to Dashboard buttons as needed

--- django/job_manager/urls.py
+++ django/job_manager/urls.py
@@ -3,7 +3,7 @@ from job_manager import views
 
 app_name = "job_manager"
 urlpatterns = [
-    path('', views.job_list, name='job_list'),
+    path('', views.job_list, name='list'),
     path('', include('job_manager.api.urls')),
     path('<int:job_pk>/submit_job/', views.submit_job, name='submit_job'),
     path('job/<int:pk>', views.JobView.as_view(), name="job")
