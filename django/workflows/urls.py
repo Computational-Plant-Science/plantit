594260c49b8df078471b959e6a70c1e28e4aef40
Chris Cotter <cotter@uga.edu>
2019-03-05 16:24:30 -0500

    Bug Fixes
    - Created static files finder to get workflow static files from django
    directory
    - Fixed workflow icons location in template
    - Updated dirt2d to work with process_sample paradigm

--- django/workflows/urls.py
+++ django/workflows/urls.py
@@ -4,6 +4,5 @@ from workflows import views
 app_name = "workflows"
 urlpatterns = [
     path('', views.list_workflows, name="list"),
-    path('awesome_workflow/', include('workflows.awesome_workflow.django.urls'), name='awesome_workflow'),
-    path('dirt2d/', include('workflows.DIRT2D.django.urls'), name='dirt2d')
+    path('dirt2d/', include('workflows.dirt2d.django.urls'), name='dirt2d')
 ]
