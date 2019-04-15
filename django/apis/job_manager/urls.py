687a6ee55434b67868e174c0598ca66ccf65886a
Chris Cotter <cotter@uga.edu>
2019-04-15 15:00:33 -0400

    Moved jobs api url to match other apis

--- django/apis/job_manager/urls.py
+++ django/apis/job_manager/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from . import views
 router = routers.DefaultRouter()
 
-router.register(r'jobs', views.JobViewSet)
+router.register(r'', views.JobViewSet)
 urlpatterns = [
     url('', include(router.urls)),
 ]
