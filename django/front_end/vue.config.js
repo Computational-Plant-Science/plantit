8ad1d70947bafbd6f7432451389e1fb32fcdfc46
Chris Cotter <cotter@uga.edu>
2019-04-16 09:52:01 -0400

    Added npm run watch

--- django/front_end/vue.config.js
+++ django/front_end/vue.config.js
@@ -5,5 +5,9 @@ module.exports = {
             preProcessor: 'sass',
             patterns: []
         }
+    },
+    chainWebpack(config) {
+        //Allow npm build watch to work: https://github.com/vuejs/vue-cli/issues/1120
+        config.output.filename("assets/js/[name].js");
     }
 };
