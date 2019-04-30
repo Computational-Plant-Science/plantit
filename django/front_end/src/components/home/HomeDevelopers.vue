4002299a813aa4f92938ea2db6774fd5eda157d4
Chris Cotter <cotter@uga.edu>
2019-04-30 09:17:46 -0400

    Added text for developers

--- django/front_end/src/components/home/HomeDevelopers.vue
+++ django/front_end/src/components/home/HomeDevelopers.vue
@@ -1,7 +1,7 @@
 <template>
     <b-container fluid class="m-0 p-4 center-container">
         <h1>DEVELOPERS</h1>
-        We've made it so easy for you.
+        <h4>We have made it so easy for you.</h4>
 
         <b-row class="m-5">
             <b-col lg="6">
@@ -16,9 +16,9 @@
                         ></b-img>
                     </b-col>
                     <b-col>
-                        <h3>Focus on your algorithm.</h3>
-                        Plant IT automatically paraleizes the analysi across all
-                        samples, allowing devlopers to fous on thier alogrithms,
+                        <h3>Focus on your algorithm</h3>
+                        Plant IT automatically parallelizes the analysis across all
+                        samples, allowing you to focus on your algorithms,
                         not logistics.
                     </b-col>
                 </b-row>
@@ -34,10 +34,9 @@
                         ></b-img>
                     </b-col>
                     <b-col>
-                        <h3>Focus on your algorithm.</h3>
-                        Plant IT automatically paraleizes the analysi across all
-                        samples, allowing devlopers to fous on thier alogrithms,
-                        not logistics.
+                        <h3>UI Included</h3>
+                        Plant IT provides a UI to help users run the algorithms and set algorithm parameters.
+                        Parameters are automatically validated and passed to your algorithm.
                     </b-col>
                 </b-row>
 
@@ -52,10 +51,10 @@
                         ></b-img>
                     </b-col>
                     <b-col>
-                        <h3>Focus on your algorithm.</h3>
-                        Plant IT automatically paraleizes the analysi across all
-                        samples, allowing devlopers to fous on thier alogrithms,
-                        not logistics.
+                        <h3>No Need to Rewrite Your Existing Code</h3>
+                        You can import libraries and run the analysis as python
+                        code inside the PlantIT framework, or call
+                        an external program.
                     </b-col>
                 </b-row>
             </b-col>
@@ -72,10 +71,10 @@
                         ></b-img>
                     </b-col>
                     <b-col>
-                        <h3>Focus on your algorithm.</h3>
-                        Plant IT automatically paraleizes the analysi across all
-                        samples, allowing devlopers to fous on thier alogrithms,
-                        not logistics.
+                        <h3>Autoformatted results</h3>
+                        You choose what data to return to the user, then
+                        Plant IT automatically collates the results across all samples.
+                        Key-value pairs are return as a CSV, output files are organized by sample.
                     </b-col>
                 </b-row>
 
@@ -90,10 +89,11 @@
                         ></b-img>
                     </b-col>
                     <b-col>
-                        <h3>Focus on your algorithm.</h3>
-                        Plant IT automatically paraleizes the analysi across all
-                        samples, allowing devlopers to fous on thier alogrithms,
-                        not logistics.
+                        <h3>You control the enviroment</h3>
+                        You do not have to worry about
+                        if the system your code is running on has all the
+                        right libraries. All your algorithm code is run inside a
+                        Singularity container you provide.
                     </b-col>
                 </b-row>
             </b-col>
