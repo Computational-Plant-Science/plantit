cd5de28f876a1428d2968e39e43fb0806044fa66
Wes Bonelli <wpb3hw@virginia.edu>
2020-02-26 17:47:13 -0500

    add pg_isready check to wait-for-postgres (psycopg2 still gets a connection failure after wait-for-postgres succeeds on TeamCity)

--- dev/wait-for-postgres.sh
+++ dev/wait-for-postgres.sh
@@ -7,7 +7,12 @@ host="$1"
 shift
 cmd="$@"
 
-until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "postgres" -c '\q'; do
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -p 5432 -U "postgres" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+until runuser -l postgres -c 'pg_isready' 2>/dev/null; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
