84c496376ab5f3185ae0efe0884e418dc9ba16b0
Wes Bonelli <wpb3hw@virginia.edu>
2020-04-27 17:07:29 -0400

    debug travis deploy

--- dev/ssh-deploy.sh
+++ dev/ssh-deploy.sh
@@ -4,14 +4,9 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 ~/.ssh/id_rsa # Allow read access to the private key
 ssh-add ~/.ssh/id_rsa # Add the private key to SSH
 
-ssh -o StrictHostKeyChecking=no -p $SSH_PORT "$SSH_USER@$SSH_HOST" <<EOF
-  cd $SSH_DIRECTORY
-  docker-compose -f docker-compose.yml -f docker-compose.prod.yml down
-EOF
-
-git config --global push.default matching
-git remote add deploy ssh://$SSH_USER@$SSH_HOST:$SSH_PORT$SSH_DIRECTORY
-git push deploy master
+#git config --global push.default matching
+#git remote add deploy ssh://$SSH_USER@$SSH_HOST:$SSH_PORT$SSH_DIRECTORY
+#git push deploy master
 
 ssh -o StrictHostKeyChecking=no -p $SSH_PORT "$SSH_USER@$SSH_HOST" <<EOF
   cd $SSH_DIRECTORY
