640e6cb99a1a94832d81101cdf10ce40319ee725
Wes Bonelli <wpb3hw@virginia.edu>
2020-04-27 12:10:41 -0400

    debug .travis.yml

--- dev/ssh-deploy.sh
+++ dev/ssh-deploy.sh
@@ -4,7 +4,7 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 id_rsa # Allow read access to the private key
 ssh-add id_rsa # Add the private key to SSH
 
-ssh @SSH_USER@$SSH_HOST -p $SSH_PORT <<EOF
+ssh -o $SSH_USER@$SSH_HOST -p $SSH_PORT <<EOF
   cd $SSH_DIRECTORY
   ./dev/deploy.sh $SSH_HOST
 EOF
