98396d223646e0b29e276e10fdb668e30be3ac5f
Wes Bonelli <wpb3hw@virginia.edu>
2020-07-26 14:14:49 -0400

    disable strict host key checking when SCPing compose file to deployment target

--- dev/deploy-ssh.sh
+++ dev/deploy-ssh.sh
@@ -5,7 +5,7 @@ chmod 600 ~/.ssh/id_rsa # Allow read access to the private key
 ssh-add ~/.ssh/id_rsa # Add the private key to SSH
 
 echo "Copying compose file..."
-scp -P $SSH_PORT docker-compose.prod.yml "$SSH_USER@$SSH_HOST:$SSH_DIRECTORY/docker-compose.prod.yml"
+scp -o StrictHostKeyChecking=no -P $SSH_PORT docker-compose.prod.yml "$SSH_USER@$SSH_HOST:$SSH_DIRECTORY/docker-compose.prod.yml"
 
 echo "Running deploy script..."
 ssh -o StrictHostKeyChecking=no -p $SSH_PORT "$SSH_USER@$SSH_HOST" <<EOF
