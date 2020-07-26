d249dd6b26134f6752b18a5ef8f8d210b7d5e5ab
Wes Bonelli <wpb3hw@virginia.edu>
2020-07-26 15:00:50 -0400

    debug deploy script

--- dev/deploy-ssh.sh
+++ dev/deploy-ssh.sh
@@ -4,7 +4,9 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 ~/.ssh/id_rsa # Allow read access to the private key
 ssh-add ~/.ssh/id_rsa # Add the private key to SSH
 
-echo "Copying compose file and deploy script..."
+echo "Copying files to deployment target..."
+scp -o StrictHostKeyChecking=no -r -P $SSH_PORT plantit "$SSH_USER@$SSH_HOST:$SSH_DIRECTORY/plantit"
+scp -o StrictHostKeyChecking=no -r -P $SSH_PORT config "$SSH_USER@$SSH_HOST:$SSH_DIRECTORY/config"
 scp -o StrictHostKeyChecking=no -P $SSH_PORT docker-compose.prod.yml "$SSH_USER@$SSH_HOST:$SSH_DIRECTORY/docker-compose.prod.yml"
 scp -o StrictHostKeyChecking=no -P $SSH_PORT dev/deploy.sh "$SSH_USER@$SSH_HOST:$SSH_DIRECTORY/deploy.sh"
 
