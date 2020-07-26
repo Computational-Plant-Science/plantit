394a51966a50d2990e1e33d36938844de48a2a7b
Wes Bonelli <wpb3hw@virginia.edu>
2020-07-26 10:56:54 -0400

    add DJANGO_ADMIN_EMAIL to .env, debug new docker-hub-dependent deployment scheme

--- dev/bootstrap.sh
+++ dev/bootstrap.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 
 nocache=0
-useprod=0
 
 while getopts 'np' opt; do
     case $opt in
         n) nocache=1 ;;
-        p) useprod=1 ;;
         *) echo 'Error in command line parsing' >&2
            exit 1
     esac
 done
 shift "$(( OPTIND - 1 ))"
 
-if [[ "$useprod" -eq 0 ]]; then
-  echo "Bootstrapping ${PWD##*/} for development..."
-  compose="docker-compose -f docker-compose.dev.yml"
-else
-  echo "Bootstrapping ${PWD##*/} for production..."
-  compose="docker-compose -f docker-compose.prod.yml"
-fi
+echo "Bootstrapping ${PWD##*/} development environment..."
+compose="docker-compose -f docker-compose.dev.yml"
 
 echo "Bringing containers down..."
 $compose down --remove-orphans
@@ -40,7 +33,6 @@ npm install
 npm run build
 cd ../..
 
-
 if [[ "$nocache" -eq 0 ]]; then
   echo "Building..."
   docker build -t computationalplantscience/plantit -f dev/dockerfiles/plantit/Dockerfile .
@@ -57,22 +49,21 @@ $compose exec plantit python manage.py migrate
 echo "Creating superuser..."
 admin_password=$(cut -d '=' -f 2 <<< "$(grep "DJANGO_ADMIN_PASSWORD" "$env_file")" )
 admin_username=$(cut -d '=' -f 2 <<< "$(grep "DJANGO_ADMIN_USERNAME" "$env_file")" )
-$compose exec plantit /code/dev/configure-superuser.sh -u "$admin_username" -p "$admin_password" -e "admin@example.com"
+admin_email=$(cut -d '=' -f 2 <<< "$(grep "DJANGO_ADMIN_EMAIL" "$env_file")" )
+$compose exec plantit /code/dev/configure-superuser.sh -u "$admin_username" -p "$admin_password" -e "$admin_email"
 
 echo "Configuring sandbox deployment target container..."
 $compose up -d sandbox
 $compose exec plantit /bin/bash /root/configure-sandbox.sh
 if [ -f config/ssh/known_hosts ]; then
   touch config/ssh/known_hosts
+  $compose exec plantit bash -c "ssh-keyscan -H sandbox >> /code/config/ssh/known_hosts"
 fi
-$compose exec plantit bash -c "ssh-keyscan -H sandbox >> /code/config/ssh/known_hosts"
 if [ ! -f config/ssh/id_rsa.pub ]; then
   ssh-keygen -b 2048 -t rsa -f config/ssh/id_rsa -N ""
+  $compose exec plantit bash -c "/code/dev/ssh-copy-id.expect"
 fi
-$compose exec plantit bash -c "/code/dev/ssh-copy-id.expect"
 
-if [[ "$useprod" -eq 0 ]]; then
-  echo "Configuring iRODS container..."
-  $compose up -d irods
-  $compose exec sandbox /bin/bash /root/wait-for-it.sh irods:1247 -- /root/configure-irods.sh
-fi
+echo "Configuring iRODS container..."
+$compose up -d irods
+$compose exec sandbox /bin/bash /root/wait-for-it.sh irods:1247 -- /root/configure-irods.sh
--- dev/deploy.sh
+++ dev/deploy.sh
@@ -3,7 +3,17 @@
 host="$1"
 compose="docker-compose -f docker-compose.prod.yml"
 
-./dev/bootstrap.sh -p
+echo "Bringing containers down..."
+$compose down --remove-orphans
+
+echo "Pulling new image definitions..."
+$compose pull
+
+echo "Building front end..."
+cd plantit/front_end || exit
+npm install
+npm run build
+cd ../..
 
 echo "Collecting static files..."
 $compose run plantit ./manage.py collectstatic --no-input
@@ -21,3 +31,25 @@ find .env -type f -exec sed -i "s/NODE_ENV=development/NODE_ENV=production/g" {}
 
 echo "Bringing containers up..."
 $compose up -d
+
+echo "Running migrations..."
+$compose exec plantit python manage.py makemigrations
+$compose exec plantit python manage.py migrate
+
+echo "Creating superuser (if one does not already exist)..."
+admin_password=$(cut -d '=' -f 2 <<< "$(grep "DJANGO_ADMIN_PASSWORD" "$env_file")" )
+admin_username=$(cut -d '=' -f 2 <<< "$(grep "DJANGO_ADMIN_USERNAME" "$env_file")" )
+admin_email=$(cut -d '=' -f 2 <<< "$(grep "DJANGO_ADMIN_EMAIL" "$env_file")" )
+$compose exec plantit /code/dev/configure-superuser.sh -u "$admin_username" -p "$admin_password" -e "$admin_email"
+
+echo "Configuring sandbox deployment target container (if not already configured)..."
+$compose up -d sandbox
+$compose exec plantit /bin/bash /root/configure-sandbox.sh
+if [ -f config/ssh/known_hosts ]; then
+  touch config/ssh/known_hosts
+  $compose exec plantit bash -c "ssh-keyscan -H sandbox >> /code/config/ssh/known_hosts"
+fi
+if [ ! -f config/ssh/id_rsa.pub ]; then
+  ssh-keygen -b 2048 -t rsa -f config/ssh/id_rsa -N ""
+  $compose exec plantit bash -c "/code/dev/ssh-copy-id.expect"
+fi
