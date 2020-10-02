895a8d08c04d8233015d626348b2fdbfc6cb700b
Wes Bonelli <wpb3hw@virginia.edu>
2020-10-02 10:23:54 -0400

    fix create-env-file script

--- scripts/create-env-file.sh
+++ scripts/create-env-file.sh
@@ -4,11 +4,19 @@ admin_password=$(python2 -c "exec(\"import random\nprint('%s' % ''.join(random.S
 secret_key=$(python2 -c "exec(\"import random\nprint('%s' % ''.join(random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789') for i in range(50)))\")")
 sql_password=$(python2 -c "exec(\"import random\nprint('%s' % ''.join(random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz0123456789') for i in range(50)))\")")
 field_encryption_key=$(python2 -c "exec(\"import cryptography.fernet\nprint('%s' % cryptography.fernet.Fernet.generate_key())\")")
+
 if [[ -z "${GITHUB_SECRET}" ]]; then
   github_secret="some_github_secret"
-  echo "Warning: Github OAuth App client secret missing"
+  echo "Warning: GITHUB_SECRET environment variable missing"
 else
   github_secret="${GITHUB_SECRET}"
+fi
+
+if [[ -z "${CAS_SERVER_URL}" ]]; then
+  cas_server_url="some_cas_server_url"
+  echo "Warning: CAS_SERVER_URL environment variable missing"
+else
+  cas_server_url="${CAS_SERVER_URL}"
 fi
 
 cat <<EOT >>".env"
@@ -26,7 +34,8 @@ DJANGO_ADMIN_PASSWORD=$admin_password
 DJANGO_SECURE_SSL_REDIRECT=False
 DJANGO_SESSION_COOKIE_SECURE=False
 DJANGO_CSRF_COOKIE_SECURE=False
-DJANGO_CAS_PROXY_CALLBACK_URL=http://localhost/accounts/callback/
+DJANGO_CAS_SERVER_URL=$cas_server_url
+DJANGO_CAS_PROXY_CALLBACK_=http://localhost/accounts/callback/
 DJANGO_CAS_FORCE_SSL_SERVICE_URL=False
 SQL_ENGINE=django.db.backends.sqlite3
 GITHUB_AUTH_URI=https://github.com/login/oauth/authorize
