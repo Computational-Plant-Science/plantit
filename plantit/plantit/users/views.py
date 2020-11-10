e57ae174fad00d93ab24e3c69d5f5b7933540a66
Wes Bonelli <wpb3hw@virginia.edu>
2020-11-10 13:17:28 -0500

    debug get all users view

--- plantit/plantit/users/views.py
+++ plantit/plantit/users/views.py
@@ -177,7 +177,6 @@ class UsersViewSet(viewsets.ModelViewSet, mixins.RetrieveModelMixin):
                     'username': user.username,
                     'first_name': user.first_name,
                     'last_name': user.last_name,
-                    'github_username': user.profile.github_username,
                 })
 
         return JsonResponse({
