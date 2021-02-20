62481e498056e3c4c9a53bb4f6418410e73194a3
Wes Bonelli <wpb3hw@virginia.edu>
2021-02-20 11:52:10 -0500

    debug daphne

--- plantit/plantit/asgi.py
+++ plantit/plantit/asgi.py
@@ -1,14 +1,15 @@
 import os
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "plantit.settings")
+asgi_app = get_asgi_application()
 
 from plantit.runs.urls import websocket_urlpatterns
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "plantit.settings")
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 
-asgi_app = get_asgi_application()
 application = ProtocolTypeRouter({
     "http": asgi_app,
     "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
