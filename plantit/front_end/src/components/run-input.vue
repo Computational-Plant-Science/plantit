60a5fee6362da282c5212a1bec8e115c08e33d9b
Wes Bonelli <wpb3hw@virginia.edu>
2021-03-24 13:47:32 -0400

    fix UI on run input component

--- plantit/front_end/src/components/run-input.vue
+++ plantit/front_end/src/components/run-input.vue
@@ -7,7 +7,10 @@
             {{ this.kind === 'files' ? 'directory' : this.kind }} from the Data
             Store.
         </b>
-        <br />
+      <hr
+                                        class="mt-2 mb-2"
+                                        style="border-color: gray"
+                                    />
         <b-tabs
             class="mt-2"
             vertical
