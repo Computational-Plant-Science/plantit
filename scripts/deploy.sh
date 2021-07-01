817531445dc28d32147acf08b0ee70fd00fe7246
Wes Bonelli <wpb3hw@virginia.edu>
2021-07-01 16:36:10 -0400

    cleanup CI config and deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -47,22 +47,22 @@ echo "Running migrations..."
 $compose exec -T plantit python manage.py makemigrations --noinput
 $compose exec -T plantit python manage.py migrate --noinput
 
-echo "Configuring deployment targets..."
-$compose exec -T plantit /bin/bash /code/scripts/configure-sandbox.sh
-$compose exec -T sandbox mkdir /test
-if [ ! -d config/ssh ]; then
-  mkdir config/ssh
-fi
-rm config/ssh/known_hosts
-touch config/ssh/known_hosts
-$compose exec -T plantit bash -c "ssh-keyscan -H sandbox >> /code/config/ssh/known_hosts"
-$compose exec -T plantit bash -c "ssh-keyscan -H stampede2.tacc.utexas.edu >> /code/config/ssh/known_hosts"
-$compose exec -T plantit bash -c "ssh-keyscan -H sapelo2-droid.gacrc.uga.edu >> /code/config/ssh/known_hosts"
-$compose exec -T plantit bash -c "/code/scripts/ssh-copy-id.expect"
-if [ ! -f config/ssh/id_rsa.pub ]; then
-  ssh-keygen -b 2048 -t rsa -f config/ssh/id_rsa -N ""
-fi
-$compose exec -T celery bash -c "mkdir ~/.ssh"
-$compose exec -T celery bash -c "cp /code/config/ssh/id_rsa.pub ~/.ssh/id_rsa.pub"
-$compose exec -T celery bash -c "cp /code/config/ssh/id_rsa ~/.ssh/id_rsa"
-$compose exec -T celery bash -c "cp /code/config/ssh/known_hosts ~/.ssh/known_hosts"
+# echo "Configuring deployment targets..."
+# $compose exec -T plantit /bin/bash /code/scripts/configure-sandbox.sh
+# $compose exec -T sandbox mkdir /test
+# if [ ! -d config/ssh ]; then
+#   mkdir config/ssh
+# fi
+# rm config/ssh/known_hosts
+# touch config/ssh/known_hosts
+# $compose exec -T plantit bash -c "ssh-keyscan -H sandbox >> /code/config/ssh/known_hosts"
+# $compose exec -T plantit bash -c "ssh-keyscan -H stampede2.tacc.utexas.edu >> /code/config/ssh/known_hosts"
+# $compose exec -T plantit bash -c "ssh-keyscan -H sapelo2-droid.gacrc.uga.edu >> /code/config/ssh/known_hosts"
+# $compose exec -T plantit bash -c "/code/scripts/ssh-copy-id.expect"
+# if [ ! -f config/ssh/id_rsa.pub ]; then
+#   ssh-keygen -b 2048 -t rsa -f config/ssh/id_rsa -N ""
+# fi
+# $compose exec -T celery bash -c "mkdir ~/.ssh"
+# $compose exec -T celery bash -c "cp /code/config/ssh/id_rsa.pub ~/.ssh/id_rsa.pub"
+# $compose exec -T celery bash -c "cp /code/config/ssh/id_rsa ~/.ssh/id_rsa"
+# $compose exec -T celery bash -c "cp /code/config/ssh/known_hosts ~/.ssh/known_hosts"
