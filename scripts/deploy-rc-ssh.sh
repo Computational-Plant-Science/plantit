95871a656b0692a67447089ae283679d09faa035
Wes Bonelli <wpb3hw@virginia.edu>
2021-07-23 12:18:38 -0400

    debug RC CI

--- scripts/deploy-rc-ssh.sh
+++ scripts/deploy-rc-ssh.sh
@@ -5,8 +5,14 @@ chmod 600 ~/.ssh/id_rsa # Allow read access to the private key
 ssh-add ~/.ssh/id_rsa # Add the private key to SSH
 
 echo "Running deploy script..."
+echo "SSH Host: $RC_SSH_HOST"
+echo "Web Host: $RC_HOST"
+echo "Port: $RC_PORT"
+echo "User: $RC_USER"
+echo "Email: $RC_EMAIL"
+echo "Directory: $RC_DIRECTORY"
 ssh -o StrictHostKeyChecking=no -p $RC_PORT "$RC_USER@$RC_SSH_HOST" <<EOF
   cd $RC_DIRECTORY
   chmod +x scripts/deploy-rc.sh
-  scripts/deploy-rc.sh $RC_HOST $EMAIL
+  scripts/deploy-rc.sh $RC_HOST $RC_EMAIL
 EOF
