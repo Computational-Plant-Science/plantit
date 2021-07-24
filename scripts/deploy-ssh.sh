7e77ab5a4df966827e185524e50a77a9127cce04
Wes Bonelli <wpb3hw@virginia.edu>
2021-07-24 13:18:22 -0400

    debug SSH config in CI

--- scripts/deploy-ssh.sh
+++ scripts/deploy-ssh.sh
@@ -5,7 +5,7 @@ chmod 600 ~/.ssh/id_rsa # Allow read access to the private key
 ssh-add ~/.ssh/id_rsa # Add the private key to SSH
 
 echo "Deploying $CONFIG"
-ssh -v -o StrictHostKeyChecking=no -p $PORT "$USER@$SSH_HOST" <<EOF
+ssh -v -o StrictHostKeyChecking=no -p $PORT -i $ "$USER@$SSH_HOST" <<EOF
   cd $DIRECTORY
   chmod +x scripts/deploy.sh
   scripts/deploy.sh $CONFIG $HOST $EMAIL
