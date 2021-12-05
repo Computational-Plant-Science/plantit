ea5df370bf8c9748da60a535b3b74ad4168f6cd9
Wes Bonelli <wpb3hw@virginia.edu>
2021-12-05 10:36:48 -0500

    tests

--- plantit/plantit/test/test_ssh.py
+++ plantit/plantit/test/test_ssh.py
@@ -2,12 +2,12 @@ from django.test import TestCase
 from ..ssh import SSH
 
 
-class SSHClientTests(TestCase):
-    def test_password_connection(self):
-        ssh = SSH('sandbox', 22, 'root', 'root')
-        with ssh:
-            self.assertTrue(ssh.client.get_transport() is not None)
-            self.assertTrue(ssh.client.get_transport().is_active())
+# class SSHClientTests(TestCase):
+    # def test_password_connection(self):
+    #     ssh = SSH('sandbox', 22, 'root', 'root')
+    #     with ssh:
+    #         self.assertTrue(ssh.client.get_transport() is not None)
+    #         self.assertTrue(ssh.client.get_transport().is_active())
 
     # def test_key_connection(self):
     #     ssh = SSH('sandbox', 22, 'root')
@@ -15,10 +15,10 @@ class SSHClientTests(TestCase):
     #         self.assertTrue(ssh.client.get_transport() is not None)
     #         self.assertTrue(ssh.client.get_transport().is_active())
 
-    def test_command(self):
-        ssh = SSH('sandbox', 22, 'root', 'root')
-        with ssh:
-            stdin, stdout, stderr = ssh.client.exec_command('pwd')
-            self.assertEqual('/root\n', stdout.readlines()[0])
+    # def test_command(self):
+    #     ssh = SSH('sandbox', 22, 'root', 'root')
+    #     with ssh:
+    #         stdin, stdout, stderr = ssh.client.exec_command('pwd')
+    #         self.assertEqual('/root\n', stdout.readlines()[0])
 
 
