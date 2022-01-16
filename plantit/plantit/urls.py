7177625814a0bb3a5b9080a6f21e10fcb4895880
Wes Bonelli <wpb3hw@virginia.edu>
2022-01-16 00:47:17 -0500

    Django swagger plugin (#256)

--- plantit/plantit/urls.py
+++ plantit/plantit/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url, include
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, re_path
 from rest_framework import routers
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from .users.views import UsersViewSet, IDPViewSet
 from .consumers import UserEventConsumer
@@ -12,10 +16,26 @@ router = routers.DefaultRouter()
 router.register('users', UsersViewSet)
 router.register('idp', IDPViewSet, basename='idp')
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="plantit API",
+      default_version='v1',
+      description="The plantit REST API.",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="wbonelli@uga.edu"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = [
                   url('', include(router.urls)),
                   # url('auth/login/', login_view),
                   # url('auth/logout/', logout_view),
+                  re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+                  re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+                  re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
                   url('agents/', include("plantit.agents.urls")),
                   url('datasets/', include("plantit.datasets.urls")),
                   url('workflows/', include("plantit.workflows.urls")),
