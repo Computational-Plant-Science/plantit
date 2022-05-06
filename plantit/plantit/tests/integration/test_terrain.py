e1fcb31b43c811402e3057e52212529bbb7c2e69
Wes Bonelli <wpb3hw@virginia.edu>
2022-05-06 07:49:16 -0400

    remove old deprecated test

--- plantit/plantit/tests/integration/test_terrain.py
+++ plantit/plantit/tests/integration/test_terrain.py
@@ -4,11 +4,3 @@ from requests import HTTPError
 
 from plantit.terrain import refresh_tokens, get_dirs_async
 from plantit.tokens import TerrainToken
-
-
-class TerrainTest(TestCase):
-    def test_refresh_tokens_throws_error_when_access_token_is_invalid(self):
-        with self.assertRaises(HTTPError) as e:
-            pass
-            # refresh_tokens('wbonelli', 'not a token')
-            # self.assertTrue('401' in str(e))
