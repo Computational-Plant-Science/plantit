f9a70442e5387517521da95f36b0938715ee5b54
Wes Bonelli <wpb3hw@virginia.edu>
2022-05-26 23:02:06 -0400

    check if migration json fields are none when converting to dict (#301 WIP)

--- plantit/plantit/queries.py
+++ plantit/plantit/queries.py
@@ -755,8 +755,8 @@ def migration_to_dict(migration: Migration) -> dict:
         'completed': None if migration.completed is None else migration.completed.isoformat(),
         'target_path': migration.target_path,
         'num_folders': migration.num_folders,
-        'storage': json.loads(migration.storage),
-        'uploads': json.loads(migration.uploads)
+        'storage': json.loads(migration.storage if migration.storage is not None else '[]'),
+        'uploads': json.loads(migration.uploads if migration.uploads is not None else '[]')
     }
 
 
