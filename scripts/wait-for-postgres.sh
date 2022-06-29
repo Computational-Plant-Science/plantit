6cf7461b79e46fffba2760de08e1ba77eeaa8fc7
Wes Bonelli <wpb3hw@virginia.edu>
2022-06-29 14:12:35 -0400

    update wait-for-postgres script, prod compose config fixes

--- scripts/wait-for-postgres.sh
+++ scripts/wait-for-postgres.sh
@@ -4,14 +4,15 @@
 set -e
 
 host="$1"
+user="$2"
 shift
 cmd="$@"
 
-until PGPASSWORD=$SQL_PASSWORD psql -h "$host" -U "postgres" -c '\q'; do
-  >&2 echo "Postgres is unavailable - sleeping"
+until PGPASSWORD=$SQL_PASSWORD psql -h "$host" -U "$user" -c '\q'; do
+  >&2 echo "Postgres is unavailable, sleeping for 1 second"
   sleep 1
 done
 
->&2 echo "Postgres is up - executing command"
+>&2 echo "Postgres is up, executing command"
 exec $cmd
 
