4bafa6ccd0e1b58d737ea3db2f3e9595b9a0dda1
w-bonelli <wesbonelli@gmail.com>
2022-09-16 20:45:18 -0400

    update test_github.py

--- plantit/plantit/tests/integration/test_github.py
+++ plantit/plantit/tests/integration/test_github.py
@@ -8,7 +8,7 @@ import plantit.github as github
 
 class GithubTests(TestCase):
     async def test_list_user_organizations(self):
-        institutions = await github.list_user_organizations('w-bonelli', environ.get('GITHUB_TOKEN'))
+        institutions = await github.list_user_organizations('w-bonelli', environ.get('GH_TOKEN'))
 
         self.assertGreater(len(institutions), 0)
         self.assertTrue('Computational-Plant-Science' in [inst['login'] for inst in institutions])
