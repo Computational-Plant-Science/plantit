fa87dd03e8476436260f0866546629d8db2e254e
w-bonelli <wesbonelli@gmail.com>
2022-09-17 07:57:01 -0400

    debug failing test

--- plantit/plantit/tests/integration/test_github.py
+++ plantit/plantit/tests/integration/test_github.py
@@ -6,9 +6,14 @@ from django.test import TestCase
 import plantit.github as github
 
 
+token = environ.get('GH_TOKEN')
+if not token:
+    token = environ.get('GITHUB_TOKEN')
+
+
 class GithubTests(TestCase):
     async def test_list_user_organizations(self):
-        institutions = await github.list_user_organizations('w-bonelli', environ.get('GH_TOKEN'))
+        institutions = await github.list_user_organizations('w-bonelli', token)
 
         self.assertGreater(len(institutions), 0)
         self.assertTrue('Computational-Plant-Science' in [inst['login'] for inst in institutions])
