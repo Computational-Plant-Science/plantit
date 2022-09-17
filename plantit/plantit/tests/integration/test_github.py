0d0d52420a04bd3f8308b745bde0884e90d299b5
w-bonelli <wesbonelli@gmail.com>
2022-09-17 08:12:53 -0400

    add GH_USERNAME env var to test environment

--- plantit/plantit/tests/integration/test_github.py
+++ plantit/plantit/tests/integration/test_github.py
@@ -11,9 +11,17 @@ if not token:
     token = environ.get('GITHUB_TOKEN')
 
 
+username = environ.get('GH_USERNAME')
+if not username:
+    username = environ.get('GITHUB_USERNAME')
+
+if not (username and token):
+    raise ValueError(f"Need github username and auth token")
+
+
 class GithubTests(TestCase):
     async def test_list_user_organizations(self):
-        institutions = await github.list_user_organizations('w-bonelli', token)
+        institutions = await github.list_user_organizations(username, token)
 
         self.assertGreater(len(institutions), 0)
         self.assertTrue('Computational-Plant-Science' in [inst['login'] for inst in institutions])
